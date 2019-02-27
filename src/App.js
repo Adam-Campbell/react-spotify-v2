2cd57a3d1dbbc92e9cd61d47a66108ad0acebc8c
Adam <adamcampbelldev@gmail.com>
2019-02-27 10:30:34 +0000

    Removed old scss directory.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import WithAuth from './components/WithAuth';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import UserProfile from './components/UserProfile';
